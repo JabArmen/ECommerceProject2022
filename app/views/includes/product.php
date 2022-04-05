8d95a13e31031bfa999d017e56f3d805d75728a4
minenice55 <star.elementa@gmail.com>
2022-04-05 11:03:17 -0400

    Create product.php

---
+++ app/views/includes/product.php
@@ -0,0 +1,39 @@
+<style>
+    .yellow-color {
+        color: rgb(246, 246, 19);
+    }
+
+    .product {
+        width: 370px;
+    }
+
+    image {
+        position: absolute;
+        right: 12px;
+        top: 10px
+    }
+</style>
+
+<div class="height d-flex">
+    <div class="card p-3 product">
+        <div class="d-flex justify-content-between align-items-center ">
+            <div class="mt-2">
+                <h4 class="text-uppercase">Text</h4>
+                <div class="mt-5">
+                    <h5 class="text-uppercase mb-0">Text</h5>
+                    <h1 class="main-heading mt-0">Text</h1>
+                    <div class="d-flex flex-row user-ratings">
+                        <div class="ratings"> <i class="fa fa-star yellow-color"></i> <i class="fa fa-star yellow-color"></i><i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> </div>
+                        <h6 class="text-muted ml-1">2/5</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="image">
+                <!-- Image --><img src="https://cdn.shopify.com/s/files/1/0609/2541/products/kr17-01ch_grande.jpg?v=1541729873" width="200">
+            </div>
+        </div>
+
+        <p>Description </p> <button class="btn btn-danger">Add to cart</button>
+    </div>
+
+</div>
